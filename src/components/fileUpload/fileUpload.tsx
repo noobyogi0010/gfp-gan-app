0ba139b8de302bf0341953713af55f3c6d8ad059
noobyogi0010 <55592987+noobyogi0010@users.noreply.github.com>
2022-01-17 13:56:24 +0530

    Remove https

--- src/components/fileUpload/fileUpload.tsx
+++ src/components/fileUpload/fileUpload.tsx
@@ -25,7 +25,7 @@ const rejectStyle = {
 // const baseUrl = " http://192.168.29.226:8080/";
 
 // base url of the development server
-const baseUrl = "https://flask-server-m2zkvmhava-em.a.run.app/";
+const baseUrl = "//flask-server-m2zkvmhava-em.a.run.app/";
 
 export default function FileUpload() {
 
